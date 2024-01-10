3f7f9918b7ddca0bf7efe5cad02b7100fc3da956
GitHub Action <action@github.com>
2024-01-10 10:18:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/nielscscholten/kotlincommentsentimentanalysis/MyBundle.kt
+++ src/main/kotlin/com/github/nielscscholten/kotlincommentsentimentanalysis/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.nielscscholten.kotlincommentsentimentanalysis
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
